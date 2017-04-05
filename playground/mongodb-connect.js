c438f7e27ceef46001c336c0477d4d1854e7d248
George Higbie <gehigbie@gmail.com>
2017-04-04 17:41:44 -0700

    Add object destructuring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,11 @@
 const MongoClient = require("mongodb").MongoClient;
 
+var user = {name: "Joe", age: 67};
+var {name} = user;
+console.log(name);
+
+
+
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     if(err){
         return console.log("Unable to connect the MongoDB server");
@@ -16,16 +22,17 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    db.collection("User").insertOne({
-        name: "George Higbie",
-        age: 38,
-        location: "Phoenix, AZ",
-
-    }, (err, result) => {
-        if(err){
-            return console.log("Unable to insert todo", err);
-        }
-        console.log(JSON.stringify(result.ops, undefined, 2));
-    })
+    // db.collection("User").insertOne({
+    //     name: "Billy Bob",
+    //     age: 45,
+    //     location: "Phoenix, AZ",
+    //
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log("Unable to insert todo", err);
+    //     }
+    //     //console.log(JSON.stringify(result.ops, undefined, 2));
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // })
     db.close();
 });
