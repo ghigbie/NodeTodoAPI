b445a60b45d05a37db12ba8b7f54b5530906b413
George Higbie <gehigbie@gmail.com>
2017-04-04 17:23:25 -0700

    Modify data entry on function

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,14 +6,26 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     }
     console.log("Connected to MongoDB server");
 
-    db.collection("Todos").insertOne({
-        text: "Something to do",
-        completed: false
+    // db.collection("Todos").insertOne({
+    //     text: "Something to do",
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log("Unable to insert todo", err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    db.collection("User").insertOne({
+        name: "George Higbie",
+        age: 38,
+        location: "Phoenix, AZ",
+
     }, (err, result) => {
         if(err){
             return console.log("Unable to insert todo", err);
         }
         console.log(JSON.stringify(result.ops, undefined, 2));
-    });
+    })
     db.close();
 });
