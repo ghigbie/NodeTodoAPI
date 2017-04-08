5a2b0536f5a201dc9941d142661822af6eb72dfa
George Higbie <gehigbie@gmail.com>
2017-04-08 15:44:34 -0700

    Add function to playground code

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,11 @@
 //const MongoClient = require("mongodb").MongoClient;
-const {MongoClient} = require("mongodb");
+const {MongoClient, ObjectID} = require("mongodb");
 
 var user = {name: "Joe", age: 67};
 var {name} = user;
 console.log(name);
 
+//the first argument for a mongoClient.connect cold be any url from any site
 
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
@@ -18,22 +19,21 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
     //     completed: false
     // }, (err, result) => {
     //     if(err){
-    //         return console.log("Unable to insert todo", err);
+    //         return console.log("Unable to insert todo.", err);
     //     }
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    // db.collection("User").insertOne({
-    //     name: "Billy Bob",
-    //     age: 45,
-    //     location: "Phoenix, AZ",
-    //
-    // }, (err, result) => {
-    //     if(err){
-    //         return console.log("Unable to insert todo", err);
-    //     }
-    //     //console.log(JSON.stringify(result.ops, undefined, 2));
-    //     console.log(result.ops[0]._id.getTimestamp());
-    // })
+    db.collection("Users").insertOne({
+        name: "Bobby McGee",
+        age: 43,
+        location: "New York, NY"
+    }, (err, result) => {
+        if(err){
+            return console.log("Unable to insert user", err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+
     db.close();
 });
