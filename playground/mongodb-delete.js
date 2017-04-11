215fdcb5b846419f06e1e404108c39964462895e
George Higbie <gehigbie@gmail.com>
2017-04-10 17:22:26 -0700

    Add method for clearing user from db

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -10,6 +10,9 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db)  => {
         // db.collection("Todos").deleteMany({text: "Something to do"}).then((result) => {
         //     console.log(result);
         // });
+    db.collection("User").deleteOne({_id: ObjectId("58e4388b8853b92ae9c68bd8")}).then((result) => {
+        console.log(result);
+    });
 
     //deleteOne
         // db.collection("Todos").deleteOne({text: "Eat lunch"}).then((result) => {
