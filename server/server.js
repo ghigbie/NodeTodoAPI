77193907556a4278eff4b2bd7f68dde259736686
George Higbie <gehigbie@gmail.com>
2017-04-10 19:10:49 -0700

    Add new model with validation

--- server/server.js
+++ server/server.js
@@ -7,26 +7,48 @@ var Todo = mongoose.model("Todo", {
     text: {
         type: String,
         required: true,
-        minlength: 1
+        minlength: 1,
+        trim: true
     },
 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
 
     completedAt: {
-        type: Number
+        type: Number,
+        defualt: null
     }
 });
 
-var newTodo = new Todo({
-    text: "",
-    completed: false,
-    completedAt: 5
+var User = mongoose.model("User", {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
 });
 
-newTodo.save().then((doc) => { //this method saves the todo to the database
-    console.log("Saved todo", doc);
+// var newTodo = new Todo({
+//     text: "    Meow at the moon     ",
+//     completed: false,
+//     completedAt: 5
+// });
+
+var newUser = new User({
+    email: "    adfada@email.com    "
+})
+
+// newTodo.save().then((doc) => { //this method saves the todo to the database
+//     console.log("Saved todo", doc);
+// }, (e) => {
+//     console.log("Unable to save todo", e)
+// });
+
+newUser.save().then((doc) => {
+    console.log("Saved user", doc);
 }, (e) => {
-    console.log("Unable to save todo", e)
+    console.log("Unable to save user", e)
 });
