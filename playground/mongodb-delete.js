67ca139211286a86474377abe70af8993bb7411c
George Higbie <gehigbie@gmail.com>
2017-04-10 17:16:50 -0700

    Add findone and delete method

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -7,16 +7,19 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db)  => {
     console.log("Connected to MongoDB server");
 
     //deteleMany
-        db.collection("Todos").deleteMany({text: "Something to do"}).then((result) => {
-            console.log(result);
-        });
+        // db.collection("Todos").deleteMany({text: "Something to do"}).then((result) => {
+        //     console.log(result);
+        // });
 
     //deleteOne
-        db.collection("Todos").deleteOne({text: "Eat lunch"}).then((result) => {
-            console.log(result);
-        });
+        // db.collection("Todos").deleteOne({text: "Eat lunch"}).then((result) => {
+        //     console.log(result);
+        // });
 
     //findOneAndDelete
+    db.collection("Todos").findOneAndDelete({completed: false}).then((result) => {
+            console.log(result);
+    });
 
     //db.close()
 });
