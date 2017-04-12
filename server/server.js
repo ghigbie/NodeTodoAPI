44687e2215871b595593b2c0eb4c1d02cb1a2b1e
George Higbie <gehigbie@gmail.com>
2017-04-12 14:53:27 -0700

    Add app.listen

--- server/server.js
+++ server/server.js
@@ -1,3 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./model/todo");
 const {User} = require("./model/user");
+
+const app = express();
+
+
+
+app.listen(3000, process.env.IP, () => {
+    console.log("Server is up and listening on ");
+});
