b129fb25fca0504a8efa0efc57723987a3178a55
George Higbie <gehigbie@gmail.com>
2017-04-12 14:40:06 -0700

    Add model to todo.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,4 @@
-const mongoose = require("mongoose");
-
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
-
+const {mongoose} = require("./db/mongoose");
 var Todo = mongoose.model("Todo", {
     text: {
         type: String,
