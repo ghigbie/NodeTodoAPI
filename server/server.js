e1ea31cd42aa08d868715c86371b116e81f52466
George Higbie <gehigbie@gmail.com>
2017-04-12 14:58:30 -0700

    Fix model path name

--- server/server.js
+++ server/server.js
@@ -2,12 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const {mongoose} = require("./db/mongoose");
-const {Todo} = require("./model/todo");
-const {User} = require("./model/user");
+const {Todo} = require("./models/todo");
+const {User} = require("./models/user");
 
 const app = express();
 
+app.use(bodyParser.json());
 
+app.post("/todos", (req, res) => {
+    console.log(req.body);
+});
+
+app.get("*", (req, res) => {
+    res.show("This page is not found");
+});
 
 app.listen(3000, process.env.IP, () => {
     console.log("Server is up and listening on ");
