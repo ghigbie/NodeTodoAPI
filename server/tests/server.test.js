0082eb05d1087c1a47e01fe5d21b45aedfc357d8
George Higbie <gehigbie@gmail.com>
2017-05-01 18:27:02 -0700

    Modify test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,26 +4,26 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
-describe("POST / todos", () => {
+describe("POST /todos", () => {
     it("should create a new todo", (done) => {
         let text = "Test todo text";
 
-        request(app)
-            .post("/todos")
-            .send({text}) //E6 syntax for objects
-            .expect(200)
-            .exepct((res) => {
-                expect(res.body.text).toBe(text);
-            })
-            .end((err, res) => {
-                if(err) {
-                    return done(err);
-                }
-                Todo.find().then(() = > {
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
-                    done();
-                }).catch((e) => done(e));
-            });
+    request(app)
+        .post("/todos")
+        .send({text})
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.text).toBe(text);
+        })
+        end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+            }).catch((e) => done(e));
+        });
     });
 });
